b9b67bf69360b88ca5d09f7146c78ca1c606c336
Oleg Belov <belov@ymail.com>
2023-01-10 21:34:07 -0300

    Update string_print_formatted_rjust.py

--- HackerRank basic scripts/string_print_formatted_rjust.py
+++ HackerRank basic scripts/string_print_formatted_rjust.py
@@ -3,7 +3,7 @@ def print_formatted(number):
     for i in range(1,number+1):
         d = str(i).rjust(max_width)
         o = str(oct(i)[2:]).rjust(max_width)
-        h = str(hex(i)[2:]).rjust(max_width)
+        h = str(hex(i)[2:]).upper().rjust(max_width)
         b = str(bin(i)[2:]).rjust(max_width)
         print(d,o,h,b) 
 
