7d315d0b0ead6e320ff403c890bbc74b9c0481c4
Oleg Belov <belov@ymail.com>
2023-01-12 22:34:01 -0300

    Update even-odd-index-string.py

--- HackerRank basic scripts/30 Days of Code/Day 6: Let's Review/even-odd-index-string.py
+++ HackerRank basic scripts/30 Days of Code/Day 6: Let's Review/even-odd-index-string.py
@@ -1,14 +1,6 @@
 if __name__ == '__main__':
     T = int(input())
    
-
 for i in range(T):
-    string = str(input())
-    strEven = strOdd = ""
-    for j in range(len(string)):
-        if (j % 2 == 0):
-            strEven += string[j]
-        else:
-            strOdd += string[j]
-    print(f"{strEven} {strOdd}")
-    
+    string = input()
+    print(string[0::2],string[1::2])
