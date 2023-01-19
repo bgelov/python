9e4760db2dce26ca65db5b88ac53ec6b6f4bde7b
Oleg Belov <belov@ymail.com>
2023-01-19 16:09:39 -0300

    Create mod-divmod.py

---
+++ module/math/mod-divmod.py
@@ -0,0 +1,7 @@
+#https://www.hackerrank.com/challenges/python-mod-divmod/problem
+
+a = int(input())
+b = int(input())
+print(a//b) #integer division
+print(a%b) #result of the modulo operator
+print(divmod(a,b)) #integer division and result of the modulo operator
