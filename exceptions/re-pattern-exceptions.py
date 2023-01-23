78d5c0bc079cdf36e1aa97298e169c8ba157cd50
Oleg Belov <belov@ymail.com>
2023-01-22 23:10:32 -0300

    Create re-pattern-exceptions.py

---
+++ exceptions/re-pattern-exceptions.py
@@ -0,0 +1,11 @@
+#https://www.hackerrank.com/challenges/incorrect-regex/problem
+import re
+
+T = int(input())
+for i in range(T):
+    S = input()
+    try:
+        pattern = re.compile(S)
+        print("True")
+    except:
+        print("False")
