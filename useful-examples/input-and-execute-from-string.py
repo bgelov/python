24a5bb2abce3dc16033507c3d05298c2f01d693c
Oleg Belov <belov@ymail.com>
2023-01-24 14:56:05 -0300

    Create input-and-execute-from-string.py

---
+++ useful-examples/input-and-execute-from-string.py
@@ -0,0 +1,19 @@
+from collections import deque
+#https://docs.python.org/3/library/collections.html#deque-objects
+
+deqCollect = deque()
+
+n = int(input())
+
+for i in range(n):
+    command = input().split()
+    if command[0] == "append":
+        deqCollect.append(command[1])
+    elif command[0] == "appendleft":
+        deqCollect.appendleft(command[1])
+    elif command[0] == "pop":
+        deqCollect.pop()
+    elif command[0] == "popleft":
+        deqCollect.popleft()
+
+[print(x, end=" ") for x in deqCollect]
