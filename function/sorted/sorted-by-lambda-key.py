eba9cd7a4ea488e9e9a0f16402229c24d225400a
Oleg Belov <belov@ymail.com>
2023-01-26 16:43:33 -0300

    Create sorted-by-lambda-key.py

---
+++ function/sorted/sorted-by-lambda-key.py
@@ -0,0 +1,27 @@
+#https://www.hackerrank.com/challenges/python-sort-sort/problem
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+if __name__ == '__main__':
+    nm = input().split()
+
+    n = int(nm[0])
+
+    m = int(nm[1])
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    k = int(input())
+
+result = sorted(arr, key=lambda x: x[k])
+
+for string in result:
+    print(*string)
