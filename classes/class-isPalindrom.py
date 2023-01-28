85698f6185d3cf20de8156ebdd2a9427537121ad
Oleg Belov <belov@ymail.com>
2023-01-28 15:41:05 -0300

    isPalindrom class

---
+++ classes/class-isPalindrom.py
@@ -0,0 +1,9 @@
+#Given an integer x, return true if x is a palindrome, and false otherwise.
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        x = str(x)
+        if x==x[::-1]:
+            return True
+        else:
+            return False
