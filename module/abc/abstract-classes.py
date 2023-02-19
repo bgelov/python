f592f34f11004de2858b0c02b9fa05681cf70772
Oleg Belov <belov@ymail.com>
2023-02-19 17:14:28 -0300

    Change code style

--- module/abc/abstract-classes.py
+++ module/abc/abstract-classes.py
@@ -1,25 +1,30 @@
-#https://www.hackerrank.com/challenges/30-abstract-classes/problem
+# https://www.hackerrank.com/challenges/30-abstract-classes/problem
 
 from abc import ABCMeta, abstractmethod
+
+
 class Book(object, metaclass=ABCMeta):
-    def __init__(self,title,author):
-        self.title=title
-        self.author=author   
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+
     @abstractmethod
     def display(): pass
-        
+
+
 class MyBook(Book):
     def __init__(self, title, author, price):
         super().__init__(title, author)
-        self.price = price        
-    def display(self):
-        print("Title:",self.title)
-        print("Author:",self.author)
-        print("Price:",self.price)
-    
+        self.price = price
 
-title=input()
-author=input()
-price=int(input())
-new_novel=MyBook(title,author,price)
+    def display(self):
+        print("Title:", self.title)
+        print("Author:", self.author)
+        print("Price:", self.price)
+
+
+title = input()
+author = input()
+price = int(input())
+new_novel = MyBook(title, author, price)
 new_novel.display()
